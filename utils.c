e1bf1ce122df4a159f1191a173559c5f4e27e8a2
Renan <silva.renanleite@gmail.com>
2023-09-11 20:57:38 -0300

    add: ft_memmove
    used to do a normalizer on input

--- utils.c
+++ utils.c
@@ -171,3 +171,17 @@ int	ft_atoi(const char *nptr)
 		num = (num * 10) + (*nptr++ - '0');
 	return (num * signal);
 }
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	size_t	index;
+
+	index = -1;
+	if (dest <= src)
+		while (++index < n)
+			((unsigned char *)dest)[index] = ((unsigned char *)src)[index];
+	else
+		while (n--)
+			((unsigned char *)dest)[n] = ((unsigned char *)src)[n];
+	return (dest);
+}
