d9f50c8f993761c85db5499bdc8e8bea45a96466
Renan <silva.renanleite@gmail.com>
2023-09-11 18:42:43 -0300

    add: atoi function

--- utils.c
+++ utils.c
@@ -146,3 +146,28 @@ void	ft_free_matrix(void **matrix)
 		free(matrix);
 	}
 }
+
+static int	ft_isspace(char c)
+{
+	return (c == ' '
+		|| c == '\f'
+		|| c == '\n'
+		|| c == '\r'
+		|| c == '\t'
+		|| c == '\v');
+}
+
+int	ft_atoi(const char *nptr)
+{
+	short int	signal;
+	int			num;
+
+	num = 0;
+	while (ft_isspace(*nptr))
+		nptr++;
+	signal = (*nptr == '+' || ft_isdigit(*nptr)) - (*nptr == '-');
+	nptr += (*nptr == '+' || *nptr == '-');
+	while (ft_isdigit(*nptr))
+		num = (num * 10) + (*nptr++ - '0');
+	return (num * signal);
+}
