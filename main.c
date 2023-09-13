6d4912858ce3f8acf881b2465cbc6c25c6e94bb0
Renan <silva.renanleite@gmail.com>
2023-09-13 13:51:41 -0300

    add: map funtionalities
    Too bad Fabinho, but I don't have time to do a real good commit

--- main.c
+++ main.c
@@ -256,21 +256,193 @@ char	**do_the_map(char *file_path, int map_lines)
 	return (map);
 }
 
+char	*do_the_new_line(char *line, size_t new_size)
+{
+	size_t	line_size;
+	char	*new_line;
+
+	line_size = ft_strlen(line);
+	new_line = malloc(new_size + 1);
+	// Verificar erro
+	ft_memmove(new_line, line, line_size);
+	ft_memset(new_line + line_size, ' ', new_size - line_size);
+	new_line[new_size] = 0;
+	free(line);
+	/* while (line_size < new_size)
+		new_line[line_size++] = ' ';
+	new_line[line_size] = 0; */
+	return (new_line);
+}
+
+void	map_normalizer(char **map)
+{
+	size_t	max_line_size;
+	size_t	index;
+	char	**start;
+
+	max_line_size = 0;
+	start = map;
+	while (*map)
+	{
+		index = 0;
+		while (map[0][index])
+		{
+			if (map[0][index] == '\n' || map[0][index] == '\r')
+				map[0][index] = 0;
+			if (map[0][index] && index + 1 > max_line_size)
+				max_line_size = index + 1;
+			index++;
+		}
+		map++;
+	}
+	while (*start)
+	{
+		if (ft_strlen(*start) < max_line_size)
+			*start = do_the_new_line(*start, max_line_size);
+		start++;
+	}
+}
+
+int	validate_map(char **map)
+{
+	size_t	line_index;
+	size_t	column_index;
+
+	// Valid size
+	if (ft_matrixlen(map) < 3 || ft_strlen(map[0]) < 3)
+	{
+		puts("Make the L! 11");
+		return (-1);
+	}
+
+	// Valid characters
+	line_index = 0;
+	while (map[line_index])
+	{
+		column_index = 0;
+		while (map[line_index][column_index])
+		{
+			if (!ft_strchr("0 1NSWE", map[line_index][column_index]))
+			{
+				puts("Make the L 8!");
+				return (-1);
+			}
+			column_index++;
+		}
+		line_index++;
+	}
+	// More than one player
+	int	players = 0;
+
+	line_index = 0;
+	while (map[line_index])
+	{
+		column_index = 0;
+		while (map[line_index][column_index])
+		{
+			if (ft_strchr("NSWE", map[line_index][column_index]))
+				players++;
+			column_index++;
+		}
+		if (players > 1)
+			break ;
+		line_index++;
+	}
+	if (players != 1)
+	{
+		puts("Make the L 9!");
+		return (-1);
+	}
+	// Map surrounded by walls
+	line_index = 0;
+
+	while (map[line_index])
+	{
+		column_index = 0;
+		while (map[line_index][column_index])
+		{
+			if ((map[line_index][column_index] == '0' || ft_strchr("NSEW", map[line_index][column_index])) 
+				&& (
+					(line_index == 0 || !map[line_index + 1])// Linha zero ou linha final
+					|| (column_index == 0)
+					|| (!map[line_index][column_index + 1] || map[line_index][column_index + 1] == ' ') // Próximo é inválido
+					|| (column_index > 0 && (!map[line_index][column_index - 1] || map[line_index][column_index - 1] == ' ')) // Anterior é inválido
+				))
+			{
+				puts("Make the L! 10");
+				return (-1);
+			}
+			column_index++;
+		}
+		line_index++;
+	}
+	return (0);
+}
+
+void	adjust_attributes(char *textures[])
+{
+	int	i;
+
+	i = 0;
+	while (i < 6)
+	{
+		if (*textures[i] == 'N' || *textures[i] == 'S' || *textures[i] == 'W' || *textures[i] == 'E')
+			ft_memmove(textures[i], textures[i] + 3, ft_strlen(textures[i] + 3) + 1);
+		else if (*textures[i] == 'F' || *textures[i] == 'C')
+			ft_memmove(textures[i], textures[i] + 2, ft_strlen(textures[i] + 2) + 1);
+		++i;
+	}
+}
+
+void	get_view_attributes(char *textures[], char *filename)
+{
+	int	fd;
+	int	i;
+	int	j;
+	const char	*elements[6] = {"NO ", "SO ", "WE ", "EA ", "F ", "C "};
+	char	*line;
+
+	fd = open(filename, O_RDONLY);
+	i = 0;
+	while (i < 6)
+	{
+		line = get_next_line(fd);
+		if (line[0] == '\n')
+			free(line);
+		else
+		{
+			normalize_element(line);
+			j = 0;
+			while (ft_strncmp(line, elements[j], ft_strlen(elements[j])) != 0)
+				++j;
+			textures[j] = line;
+			++i;
+		}
+	}
+	close(fd);
+	adjust_attributes(textures);
+}
+
 int	main(int argc, char *argv[])
 {
 	int		map_lines;
 	char	**map;
+	char	*textures[6];
 
 	if (error_checker(argc, argv))
 		return (1);
 	if (element_checker(argv[1], map))
 		return (1);
-	map_lines = coun_map_lines(argv[1]);
+	// Validar texturas das Coordenadas
+	get_view_attributes(textures, argv[1]);
+	map_lines = count_map_lines(argv[1]);
 	if (map_lines < 0)
 		return (2);
 	map = do_the_map(argv[1], map_lines);
 	if (!map)
 		return (1);
+	map_normalizer(map);
+	validate_map(map);
 	print_map(map);
 	return (0);
 }
