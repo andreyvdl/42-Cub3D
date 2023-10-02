e83785ebf128b36be631b0209e81c8343c09c0b0
adantas- <adantas-@student.42sp.org.br>
2023-10-02 17:50:59 -0300

    refactor: added new macro, changed colors, and reduce functions
    created a function to convert degrees in radians
    created a function to add (or subtract) player position on X & Y
    colors are similar to the main on the branch start
    functions draw_direction and keyboard, have been refactored

--- cub_enviesado.c
+++ cub_enviesado.c
@@ -4,6 +4,7 @@
 #include <math.h>
 
 #define SIZE 16
+#define ROT_ANG 11
 
 float g_player_x = 3 * SIZE; // posição inicial do player
 float g_player_y = 3 * SIZE;
@@ -28,6 +29,11 @@ const char	*g_map[] = {
 	"11111111",
 	NULL
 };
+
+float	deg_to_rad(float deg)
+{
+	return (-(deg * M_PI / 180));
+}
 
 void	draw_background(t_mlx *mlx)
 {
@@ -55,56 +61,57 @@ void	draw_map(t_mlx *mlx)
 {
 	for (int y = 0; g_map[y]; ++y) {
 		for (int x = 0; g_map[y][x]; ++x) { // esses dois for olham o mapa
-			for (int i = y * 32 + 1; i < y * 32 + 32 ; ++i) {
-				for (int j = x * 32 + 1; j < x * 32 + 32; ++j) { // esses definem o limite do que vão desenhar
+			for (int i = y * SIZE + 1; i < y * SIZE + SIZE ; ++i) {
+				for (int j = x * SIZE + 1; j < x * SIZE + SIZE; ++j) { // esses definem o limite do que vão desenhar
 					if (g_map[y][x] == '1')
-						mlx_put_pixel(mlx->img, j, i, 0xFFFFFFFF);
+						mlx_put_pixel(mlx->img, j, i, 0x00FF00FF);
 					else
-						mlx_put_pixel(mlx->img, j, i, 0x000000FF);
+						mlx_put_pixel(mlx->img, j, i, 0x0000FFFF);
 				}
 			}
 		}
 	}
 }
 
+	//desenha o player
 void	draw_player(t_mlx *mlx)
 {
-	//desenha o player
 	for (int i = g_player_y - 4; i < g_player_y + 4; ++i) {
 		for (int j = g_player_x - 4; j < g_player_x + 4; ++j) {
+			if (i < 0 || j < 0)
+				break ;
 			mlx_put_pixel(mlx->img, j, i, 0xFFFF00FF);
 		}
 	}
 }
 
-void	draw_direction(t_mlx *mlx)
+//desenha a linha reta da direção do player
+// o algoritmo é o DDA
+void	draw_direction(t_mlx *mlx, float x0, float y0)
 {
-	//desenha a linha reta da direção do player
-	// o algoritmo é o DDA
 	float	dist_x;
 	float	dist_y;
-	float	x;
-	float	y;
 	float	step;
 
-	dist_x = g_dir_x * 5 + g_player_x - g_player_x;
-	dist_y = g_dir_y * 5 + g_player_y - g_player_y;
+	dist_x = g_dir_x * SIZE / 2; // multiplicação define a distancia do vetor
+	dist_y = g_dir_y * SIZE / 2;
 	if (fabs(dist_x) > fabs(dist_y))
 		step = fabs(dist_x);
 	else
 		step = fabs(dist_y);
 	dist_x /= step;
 	dist_y /= step;
-	x = g_player_x;
-	y = g_player_y;
-	for (int i = 0; i <= step; ++i) {
-		mlx_put_pixel(mlx->img, (int)x, (int)y, 0xFF0000FF);
-		x += dist_x;
-		y += dist_y;
+	while ((int)step--)
+	{
+		if (x0 < 0 || y0 < 0)
+			break ;
+		mlx_put_pixel(mlx->img, x0, y0, 0xFFFF00FF);
+		x0 += dist_x;
+		y0 += dist_y;
 	}
 }
 
-void	draw_ray(t_mlx *mlx) // WIP
+/* void	draw_ray(t_mlx *mlx) // WIP
 {
 	float	ray_y;
 	float	ray_x;
@@ -145,7 +152,7 @@ void	draw_ray(t_mlx *mlx) // WIP
 			if (map_point < 8 * 8 && g_map[map_point] == '1') // mapa dele é plano, tem q converter isso pra matriz
 		}
 	}
-}
+} */
 
 void	render(void *var)
 {
@@ -155,43 +162,43 @@ void	render(void *var)
 	draw_background(mlx);
 	draw_map(mlx);
 	draw_player(mlx);
-	draw_direction(mlx);
+	draw_direction(mlx, g_player_x, g_player_y);
 	// draw_ray(mlx);
-	mlx_image_to_window(mlx->win, mlx->img, 0, 0);
+}
+
+void	change_pos(float x, float y)
+{
+	if (g_player_x + x > 0.9)
+		g_player_x += x;
+	if (g_player_y + y > 0.9)
+		g_player_y += y;
 }
 
 void	keyboard(mlx_key_data_t data, void *var)
 {
 	t_mlx	*mlx;
 
+	(void)data;
 	mlx = (t_mlx *)var;
-	if (data.key == MLX_KEY_A && data.action > MLX_RELEASE)
+	if (mlx_is_key_down(mlx->win, MLX_KEY_LEFT))
 	{
-		g_player_angle -= 0.1;
+		g_player_angle -= ROT_ANG;
 		if (g_player_angle < 0)
-			g_player_angle += 2 * M_PI;
-		g_dir_x = cos(g_player_angle) * 5; // altera a rotação do player,
-		g_dir_y = sin(g_player_angle) * 5; // permite andar livremente
+			g_player_angle += 359;
 	}
-	else if (data.key == MLX_KEY_D && data.action > MLX_RELEASE)
-	{
-		g_player_angle += 0.1;
-		if (g_player_angle > 2 * M_PI)
-			g_player_angle -= 2 * M_PI;
-		g_dir_x = cos(g_player_angle) * 5;
-		g_dir_y = sin(g_player_angle) * 5;
-	}
-	else if (data.key == MLX_KEY_W && data.action > MLX_RELEASE)
-	{
-		g_player_x += g_dir_x;
-		g_player_y += g_dir_y;
-	}
-	else if (data.key == MLX_KEY_S && data.action > MLX_RELEASE)
-	{
-		g_player_y -= g_dir_y;
-		g_player_x -= g_dir_x;
-	}
-	else if (data.key == MLX_KEY_ESCAPE && data.action == MLX_PRESS)
+	if (mlx_is_key_down(mlx->win, MLX_KEY_RIGHT))
+		g_player_angle = (int)(g_player_angle + ROT_ANG) % 360;
+	g_dir_x = cos(deg_to_rad(g_player_angle)) * 3; // altera a rotação do player,
+	g_dir_y = sin(deg_to_rad(g_player_angle)) * 3; // permite andar livremente
+	if (mlx_is_key_down(mlx->win, MLX_KEY_W))
+		change_pos(g_dir_x, g_dir_y);
+	if (mlx_is_key_down(mlx->win, MLX_KEY_S))
+		change_pos(-g_dir_x, -g_dir_y);
+	if (mlx_is_key_down(mlx->win, MLX_KEY_A))
+		change_pos(cos(deg_to_rad(g_player_angle + 90)) * 3, sin(deg_to_rad(g_player_angle + 90)) * 3);
+	if (mlx_is_key_down(mlx->win, MLX_KEY_D))
+		change_pos(-cos(deg_to_rad(g_player_angle + 90)) * 3, -sin(deg_to_rad(g_player_angle + 90)) * 3);
+	if (mlx_is_key_down(mlx->win, MLX_KEY_ESCAPE))
 		mlx_close_window(mlx->win);
 }
 
@@ -206,9 +213,10 @@ int	main(void)
 	mlx.img = mlx_new_image(mlx.win, 800, 600);
 	if (mlx.img == NULL)
 		return (puts(mlx_strerror(mlx_errno)), mlx_terminate(mlx.win), 2);
-	g_player_angle = -(M_PI / 2); // precisa ser negativo pra inverter o sentido
-	g_dir_x = cos(g_player_angle) * 5;
-	g_dir_y = sin(g_player_angle) * 5;
+	mlx_image_to_window(mlx.win, mlx.img, 0, 0);
+	g_player_angle = -90; // precisa ser negativo pra inverter o sentido
+	g_dir_x = cos(deg_to_rad(g_player_angle)) * 3;
+	g_dir_y = sin(deg_to_rad(g_player_angle)) * 3;
 	mlx_key_hook(mlx.win, &keyboard, &mlx);
 	mlx_loop_hook(mlx.win, &render, &mlx);
 	mlx_loop(mlx.win);
