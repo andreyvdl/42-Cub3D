15c92030f569d2a3dc08ed2aa5421a092b72c1d4
adantas- <adantas-@student.42sp.org.br>
2023-10-11 13:28:12 -0300

    fix: removed useless files

--- for_andrey_to_code_home/SDL2-2.28.4/build/.libs/SDL_render_ps2.d
+++
@@ -1,8 +0,0 @@
-build/SDL_render_ps2.lo: \
- /home/adantas-/42Projects/cub3D/SDL2-2.28.4/src/render/ps2/SDL_render_ps2.c \
- /home/adantas-/42Projects/cub3D/SDL2-2.28.4/src/render/ps2/../../SDL_internal.h \
- /home/adantas-/42Projects/cub3D/SDL2-2.28.4/src/render/ps2/../../dynapi/SDL_dynapi.h \
- /home/adantas-/42Projects/cub3D/SDL2-2.28.4/src/render/ps2/../../dynapi/SDL_dynapi_overrides.h \
- include/SDL_config.h include/SDL_platform.h include/begin_code.h \
- include/close_code.h include/SDL_assert.h include/SDL_stdinc.h \
- include/SDL_config.h include/SDL_log.h
--- for_andrey_to_code_home/SDL2-2.28.4/build/SDL_cpuinfo.d
+++
@@ -1,8 +0,0 @@
-build/SDL_cpuinfo.lo: \
- /home/adantas-/42Projects/cub3D/SDL2-2.28.4/src/cpuinfo/SDL_cpuinfo.c \
- /home/adantas-/42Projects/cub3D/SDL2-2.28.4/src/cpuinfo/../SDL_internal.h \
- /home/adantas-/42Projects/cub3D/SDL2-2.28.4/src/cpuinfo/../dynapi/SDL_dynapi.h \
- /home/adantas-/42Projects/cub3D/SDL2-2.28.4/src/cpuinfo/../dynapi/SDL_dynapi_overrides.h \
- include/SDL_config.h include/SDL_platform.h include/begin_code.h \
- include/close_code.h include/SDL_assert.h include/SDL_stdinc.h \
- include/SDL_config.h include/SDL_log.h include/SDL_cpuinfo.h
